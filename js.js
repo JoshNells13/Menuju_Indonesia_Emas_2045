f2a928c3204872cd5612cd8dd11e9e308a550793
Joshua CL <joshua.plk08@gmail.com>
2024-11-18 08:19:29 +0700

    dark Toggle

--- js.js
+++ js.js
@@ -1,15 +1,18 @@
 let menu = document.getElementById("menu-toggle")
-
+let pilar = document.getElementById("pilar")
 
 
 menu.style.height = "0px"
-
 function toggle(){
     if(menu.style.height == "0px"){
         menu.style.height = "150px"
     }else{
         menu.style.height = "0px"
     }
+}
+
+function toggledark(){
+    pilar.classList ="darkTheme"
 }
 
 
