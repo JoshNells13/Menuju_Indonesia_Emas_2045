53bd9d5cf23bce97e1dd18364d073f3ebfa37cb6
Joshua CL <joshua.plk08@gmail.com>
2024-11-18 17:18:40 +0700

    Update Tujuan

--- js.js
+++ js.js
@@ -5,7 +5,7 @@ let menu = document.getElementById("menu-toggle")
 menu.style.height = "0px"
 function toggle(){
     if(menu.style.height == "0px"){
-        menu.style.height = "150px"
+        menu.style.height = "200px"
     }else{
         menu.style.height = "0px"
     }
